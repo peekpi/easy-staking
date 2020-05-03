c49c46982e3ecaa857a6c644f5cb9491a1ecc6a7
lixp <894646171@qq.com>
2020-05-04 00:16:19 +0800

    fuck the element close check

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,73 +1,87 @@
 <template>
   <div>
-    <button v-on:click=login>loginMsg</button>
-    <div>
-      <h3>name: {{ account.name }} </h3>
-      <h3>address: {{ account.address }} </h3>
-      <h2 v-bind:title="toOne(account.balance, 0)">balance: {{ toOne(account.balance, 18) }} ONE</h2>
-    </div>
-    <div>
-      <label>to:</label><input v-model="transferTo" placeholder="address" value="one1s5sqy8ufg5pegfzv6j4a5n879udsaasu2u5hdp"><br>
-      <label>amount:</label><input v-model="transferRAW" placeholder="amount" value="1"> ONE<br>
-      <button v-on:click="transfer">transfer</button>
-      <br>
-      {{ transferAmount }}
-    </div>
+    <Layout class="layout">
+      <Header></Header>
+      <Content>
+        <Button @click="login" type="primary">login</Button>
+        <div>
+          <h3>name: {{ account.name }}</h3>
+          <h3>address: {{ account.address }}</h3>
+          <h3 v-bind:title="toOne(account.balance, 0)">balance: {{ toOne(account.balance, 18) }} ONE</h3>
+        </div>
+        <div>
+          <label>to:</label>
+          <Input v-model="transferTo" placeholder="address" />
+          <br />
+          <label>amount:</label>
+          <Input-number :max="10" :min="1" v-model="transferRAW" />ONE
+          <br />
+          <Button type="primary" @click="transfer">transfer</Button>
+          <br />
+          {{ transferAmount }}
+        </div>
+
+      </Content>
+      <Footer></Footer>
+    </Layout>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Wallet',
-  data: function(){
+  name: "Wallet",
+  data: function() {
     return {
-      account:{},
-      transferTo:0,
-      transferAmount:0,
-      transferRAW:null,
-      counter:1
+      account: {},
+      transferTo: 0,
+      transferAmount: 0,
+      transferRAW: null,
+      counter: 1
     };
   },
-  methods:  {
-    login: async function(){
-      if(!window.harmony) alert("please install mathwallet!");
-      if(this.account.address){
+  methods: {
+    login: async function() {
+      if (!window.harmony) alert("please install mathwallet!");
+      if (this.account.address) {
         await window.harmony.forgetIdentity();
         this.account = {};
-      }else{
+      } else {
         this.account = await window.harmony.getAccount();
         const hmy = this.$root.hmy.hmy;
         let balance = await hmy.blockchain.getBalance(this.account);
         this.$set(this.account, "balance", balance.result);
       }
     },
-    toOne: function(amount, unit){
-        if(!amount) return '-';
-        const hmy = this.$root.hmy.hmy;
-        if(unit==0) return hmy.utils.hexToNumber(amount);
-        return hmy.utils.hexToNumber(amount)/(10**unit);
+    toOne: function(amount, unit) {
+      if (!amount) return "-";
+      const hmy = this.$root.hmy.hmy;
+      if (unit == 0) return hmy.utils.hexToNumber(amount);
+      return hmy.utils.hexToNumber(amount) / 10 ** unit;
     },
-    transfer: function(){
-        const hmy = this.$root.hmy;
-        let tx = hmy.transfer(this.account.address, this.transferTo, this.transferAmount);
-        console.log(tx);
-        window.tx = tx;
+    transfer: function() {
+      const hmy = this.$root.hmy;
+      let tx = hmy.transfer(
+        this.account.address,
+        this.transferTo,
+        this.transferAmount
+      );
+      window.tx = tx;
+      window.harmony.signTransaction(tx);
+      tx.sendTransaction();
     }
   },
   watch: {
-    transferRAW: function(){
-        try {
-          const hmy = this.$root.hmy.hmy;
-          this.transferAmount = hmy.utils.toWei(this.transferRAW, "one");
-        } catch {
-          this.transferAmount = 0;
-        }
+    transferRAW: function() {
+      try {
+        const hmy = this.$root.hmy.hmy;
+        this.transferAmount = hmy.utils.toWei(this.transferRAW, "one");
+      } catch {
+        this.transferAmount = 0;
+      }
     }
   },
-  computed: {
-
-  }
-}
+  computed: {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
