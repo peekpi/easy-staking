f28ab9b4c4ee55805689f3ae421b13b1ddeccc88
lixp <894646171@qq.com>
2020-05-13 23:09:10 +0800

    add rule func

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -1,7 +1,18 @@
 <template>
     <Layout>
         <Header>
-            <img v-if="true" src="../img/logo-top-right.png" class="logo"/>
+            <img v-if="true" src="../img/logo-top-right.png" class="logo" />
+            <div class="topright">
+                <Button type="text" ghost @click="enable=true">投票规则</Button>
+            </div>
+            <Modal footer-hide v-model="enable" fullscreen title="投票规则">
+              <List :split=false>
+                <ListItem v-for="(rule, index) in stakingRules" :key="index">
+                  {{ index }}. {{ rule }}
+                </ListItem>
+                </ListItem>
+              </List>
+            </Modal>
         </Header>
         <Content>
             <Account v-model="delegate" />
@@ -15,6 +26,8 @@ import ValidatorList from "@/components/ValidatorList";
 import Delegations from "@/components/Delegations";
 import Account from "@/components/Account";
 
+const stakingRules = require("../js/stakingRules.json");
+
 export default {
     name: "StakingHome",
     components: {
@@ -24,6 +37,8 @@ export default {
     },
     data() {
         return {
+            stakingRules,
+            enable: false,
             delegate: true
         };
     },
@@ -33,9 +48,17 @@ export default {
 
 <style scoped>
 .logo {
-  margin-top: 0.5em;
+    margin-top: 0.5em;
 }
 .ivu-layout-header {
-  padding: 0 1em;
+    padding: 0 0 0 1em;
+}
+.topright {
+    float: right;
+    /*position: absolute;
+    right: 0px;
+    top:0px;*/
+    line-height: 0;
+    padding: 0;
 }
 </style>
