1eef2406fea9707a81d49b0e8a49aa5fee1e95ca
lixp <894646171@qq.com>
2020-05-20 17:44:21 +0800

    remove debug info in release version

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/easy-staking/dist/'
-      : '/'
+      : '/',
+    productionSourceMap: process.env.NODE_ENV === 'production'
+      ? false : true
 }
