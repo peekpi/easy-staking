0221e53724b5773e47753bd6be6cc67eb453c9d2
lixp <894646171@qq.com>
2020-05-20 19:24:24 +0800

    load 'axios,vue,vuex' from cdn

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,22 @@
+const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/easy-staking/dist/'
-      : '/',
-    productionSourceMap: process.env.NODE_ENV === 'production'
-      ? false : true
+  publicPath: isProduction
+    ? '/easy-staking/dist/'
+    : '/',
+  productionSourceMap: isProduction
+    ? false : true,
+  configureWebpack: config => {
+    if (isProduction)
+      config.externals = {
+        'vue': 'Vue',
+        'vuex': 'Vuex',
+        'axios': 'axios'
+      }
+  },
+  /*
+  chainWebpack: config => {
+    config
+      .plugin('webpack-bundle-analyzer')
+      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+  }*/
 }
